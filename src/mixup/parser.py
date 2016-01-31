54cbcc89ea4f31fc9414ae5872563f0cd020ff9f
Dmitriy Ferens <ferensdima@gmail.com>
2016-01-31 23:15:11 +0200

    Added ability to play single class only

--- src/mixup/parser.py
+++ src/mixup/parser.py
@@ -1,52 +1,80 @@
 """
 Parse database file (see docs/db-sample.txt)
 """
-
+import re
 from .core import ClassSkill, PlayerInfo
 
+SCOUT_PATTERN = re.compile('scout')
+SOLDIER_PATTERN = re.compile('soldier')
+DEMOMAN_PATTERN = re.compile('demoman')
+MEDIC_PATTERN = re.compile('medic')
 
-def _is_medic_only(class_string):
-    return class_string in ('medic only', 'medic_only')
+
+def _is_single_class_player(class_token):
+    return 'only' in class_token
+
+
+def _get_class_name(class_token):
+    if SCOUT_PATTERN.match(class_token):
+        return ClassSkill.SCOUT
+    elif SOLDIER_PATTERN.match(class_token):
+        return ClassSkill.SOLDIER
+    elif DEMOMAN_PATTERN.match(class_token):
+        return ClassSkill.DEMOMAN
+    elif MEDIC_PATTERN.match(class_token):
+        return ClassSkill.MEDIC
+    else:
+        raise Exception('Invalid class: %s' % class_token)
 
 
 def _parse_classes_token(token):
-    return set(map(str.lower, map(str.strip, token.split(','))))
+    tokens = map(str.lower, map(str.strip, token.split(',')))
+    return set(map(_get_class_name, tokens))
 
 
 def _parse_line(line_text):
+    """
+    Parses line of players list.
+    Line is tab separated string of tokens - skill, nickname, main class
+    and string of additional classes which may be empty.
+
+    Some notes on classes:
+        - Players can choose 'scout', 'soldier', 'demoman' and 'medic' classes.
+        - Players can add 'only' string to main class so they will play only
+          one class.
+        - Prem tier players are available to play their main classes only within
+          'only' mode (see prev note).
+    """
     tokens = [t for t in line_text.split('\t') if t != '']
     skill = tokens[0].lower()
     nickname = tokens[1]
-    main_class = tokens[2].lower()
-
-    if len(tokens) < 4:
-        additional_classes = set()
-    else:
-        additional_classes = _parse_classes_token(tokens[3])
-        # Idiots check #1
-        if main_class in additional_classes:
-            additional_classes.remove(main_class)
-
+    main_class_token = tokens[2].lower()
+    main_class = _get_class_name(main_class_token)
     classes = set()
-    if _is_medic_only(main_class):
-        if skill == PlayerInfo.PREM:
-            raise Exception('Prem player can not be "medic only"')
-        else:
-            classes.add(ClassSkill(ClassSkill.MEDIC, ClassSkill.MAIN))
-    else:
-        nonmain_classes = ClassSkill.CLASSES.difference(additional_classes)
-        nonmain_classes.remove(main_class)
 
-        # Open players should not play nonmain classes
-        if skill != 'open':
-            for c in nonmain_classes:
-                classes.add(ClassSkill(c, ClassSkill.NONMAIN))
+    if _is_single_class_player(main_class_token):
+        classes.add(ClassSkill(main_class, ClassSkill.MAIN))
+    else:
+        if len(tokens) < 4:
+            additional_classes = set()
+        else:
+            additional_classes = _parse_classes_token(tokens[3])
+            # Idiots check #1
+            if main_class in additional_classes:
+                additional_classes.remove(main_class)
+
+        if skill != PlayerInfo.PREM:
+            classes.add(ClassSkill(main_class, ClassSkill.MAIN))
 
         for c in additional_classes:
             classes.add(ClassSkill(c, ClassSkill.ADDITIONAL))
 
-        if skill != PlayerInfo.PREM:
-            classes.add(ClassSkill(main_class, ClassSkill.MAIN))
+        # Open players should play main and additional classes only
+        if skill != 'open':
+            nonmain_classes = ClassSkill.CLASSES.difference(additional_classes)
+            nonmain_classes.remove(main_class)
+            for c in nonmain_classes:
+                classes.add(ClassSkill(c, ClassSkill.NONMAIN))
 
     info = PlayerInfo(nickname, skill, tuple(classes))
     return info
@@ -54,7 +82,7 @@ def _parse_line(line_text):
 
 def _parse_text(text):
     for line in text.split('\n'):
-        if not line.startswith('//'):
+        if not line.startswith('//') and line != '':
             yield _parse_line(line)
 
 
